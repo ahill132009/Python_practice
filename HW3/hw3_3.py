22fd313ce8efcd95f8af77d962d6fe9d1b264ec5
ahill132009 <sv200466@gmail.com>
2020-10-20 21:45:28 +0300

    fixed 3d assignment

--- HW3/hw3_3.py
+++ HW3/hw3_3.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import csv
 import pickle
+import random
+import json
 
 
 class UnigramMorphAnalyzer:
@@ -12,24 +14,30 @@ class UnigramMorphAnalyzer:
 		self.path_to_corpora = path_to_corpora
 		self.records = load_corpora(self.path_to_corpora)
 		self.ending_stats = {}
-		self.loaded_pickle = None
-		self.test_corpora = None
+		self.name_of_csv = 'word_pos.csv'
+		self.stopwords = {}
 
 	def __getitem__(self, idx):
 		return self.ending_stats[idx]
 
 	@staticmethod
-	def ending_of_word(word):
-		# Return ending of the word, depending on the length of the word
-		if len(word) < 4:
-			ending = word[-(len(word)):]
-		else:
-			ending = word[-4:]
-		return ending
+	def count_probability(dict_of_pos):
+		dict_with_probs = {}
+		sum_vals = sum([v for v in dict_of_pos.values()])
+		for k, v in dict_of_pos.items():
+			dict_with_probs[k] = round(v/sum_vals*100, 2)
+		return dict_with_probs		
 
-	def train(self):
+	def stopwords_foo(self, path_to_stopwords):
+		with open(path_to_stopwords, 'rb') as f:
+			self.stopwords = json.load(f)
+		print('Stopwords were written')
+		return self.stopwords
+
+	def make_csv(self, name_of_csv):
+		self.name_of_csv = name_of_csv
 		# Write csv with word-pos pairs
-		with open('corpora_words_grams.csv', 'w+', newline='') as csvfile:
+		with open(self.name_of_csv, 'w+', newline='') as csvfile:
 			writer = csv.writer(csvfile, delimiter='\t')
 			writer.writerow(['Text', 'Part of speech'])
 			for rec_part in self.records:
@@ -38,111 +46,77 @@ class UnigramMorphAnalyzer:
 						# Check for [] where [txt] should be
 							if x.forms[0].text:
 								writer.writerow([x.forms[0].text, x.forms[0].grams[0]])
+		print(f'CSV file successfully written')
 
-		df = pd.read_csv('corpora_words_grams.csv', sep='\t')
+	def split_corpora(self):
+		df = pd.read_csv(self.name_of_csv, sep='\t')
 		X = df.iloc[:, 0]
 		y = df.iloc[:, 1]
-		X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False)
+		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, shuffle=False)
+		return self.X_train, self.X_test, self.y_train, self.y_test
 
-		train_corpora = zip(X_train, y_train)
-		self.test_corpora = zip(X_test, y_test)
+	def train(self):
+		for word_train, pos_train in zip(self.X_train, self.y_train):
+			ending = word_train[-4:] if len(word_train) >= 4 else word_train
+			if self.stopwords.get(ending) is not None:
+				self.ending_stats[ending] = self.stopwords[ending]
+			for _ in range(len(ending)):
+				pos_dict = self.ending_stats.get(ending)
+				if pos_dict is None:
+					self.ending_stats[ending] = {pos_train:1}
+				else:
+					if pos_train not in pos_dict:
+						pos_dict[pos_train] = 1
+					else:
+						pos_dict[pos_train] += 1
+				ending = ending[1:]
 
-		ending_train = ''
-		for word_train, pos_train in train_corpora:
-			ending_train = self.ending_of_word(word_train)
-			while ending_train:
-				# Sometimes [] where should be [text] appear
-				if ending_train in self.ending_stats:
-					if pos_train in self.ending_stats[ending_train]:
-						self.ending_stats[ending_train][pos_train] += 1
-					if pos_train not in self.ending_stats[ending_train]:
-						self.ending_stats[ending_train][pos_train] = 1
-				elif ending_train not in self.ending_stats:
-					self.ending_stats[ending_train] = {pos_train:1}
-				wrd_letters = list(ending_train)
-				wrd_letters.remove(ending_train[0])
-				ending_train = ''.join(wrd_letters)
-		return self.ending_stats
+	def _argmax(self, argmax_ending_dict):
+		return max(argmax_ending_dict, key=argmax_ending_dict.get)
 
-	def predict(self, token):
-		predict_ending = self.ending_of_word(token)
-		try:
-			self.string = ""
-			self.sum_vals = sum([v for v in self.ending_stats[predict_ending].values()])
-			for k, v in self.ending_stats[predict_ending].items():
-				self.string += f'Present ending is {predict_ending}. ' \
-				f'The probability of being {k} is {round(v/self.sum_vals, 3)}\n'
-			return self.string
-		except KeyError:
-			return f'There is no such ending as "{predict_ending}" in the dictionary'
+	def _distrib(self, argmax_ending_dict):
+		return random.choices(list(argmax_ending_dict.keys()), weights=argmax_ending_dict.values())[0]
+
+	def predict(self, token, method):
+		assert method in ['argmax', 'distrib']
+		ending = token[-4:] if len(token) >= 4 else token
+		for _ in range(len(ending)):
+			pred = self.ending_stats.get(ending)
+			if pred is None:
+				ending = ending[1:]
+				continue
+			pred = self.count_probability(pred)
+			if method == 'argmax':
+				return self._argmax(pred)
+			else:
+				return self._distrib(pred)
 	
+	def eval(self, method):
+		assert method in ['argmax', 'distrib']
+		correct, total = 0, 0
+		for word, pos_true in zip(self.X_test, self.y_test):
+			pred = self.predict(word, method=method)
+			if pred == pos_true:
+				correct += 1
+			total += 1
+		return correct / total
+
 	def save(self, pickle_name):
 		with open(pickle_name, 'wb') as p:
 			pickle.dump(self.ending_stats, p)
-		return f'{pickle_name} is successfully written'
+		print(f'{pickle_name} is successfully written')
 
 	def load(self, pickle_name):
 		with open(pickle_name, 'rb') as p:
 			self.loaded_pickle = pickle.load(p)
-		return f'{pickle_name} is successfully loaded'
+		print(f'{pickle_name} is successfully loaded')
 
-	def eval_argmax(self):
-		ending = ''
-		self.positives = 0
-		self.negatives = 0
-		for test_word, pos in self.test_corpora:
-			if len(test_word) < 4:
-				ending = test_word[-(len(test_word)):]
-				try:
-					max_pos = max(self.ending_stats[ending], key=self.ending_stats[ending].get)
-					if pos == max_pos:
-						self.positives += 1
-					else:
-						self.negatives += 1
-				except KeyError:
-					continue
 
-			else:
-				ending = test_word[-4:]	
-				try:
-					max_pos = max(self.ending_stats[ending], key=self.ending_stats[ending].get)
-					if pos == max_pos:
-						self.positives += 1
-					else:
-						self.negatives += 1
-				except KeyError:
-					continue
 
-		return f'Accuracy of the model is: {round((self.positives/(self.positives + self.negatives) * 100), 2)} %'
 
-	def eval_distrib(self):
-		ending = ''
-		self.positives = 0
-		self.negatives = 0
-		for test_word_d, pos_d in self.test_corpora:
-			print(test_word_d, pos_d)
-			if len(test_word_d) < 4:
-				ending = test_word_d[-(len(test_word_d)):]
-				try:
-					ngood = self.ending_stats[ending][pos_d]
-					nbad = sum([v for k,v in self.ending_stats[ending].items()]) - self.ending_stats[ending][pos_d]
-					if np.random.hypergeometric(ngood, nbad, 1) == 1:
-						self.positives += 1
-					else:
-						self.negatives += 1
-				except KeyError:
-					continue
 
-			else:
-				ending = test_word_d[-4:]	
-				try:
-					ngood = self.ending_stats[ending][pos_d]
-					nbad = sum([v for k,v in self.ending_stats[ending].items()]) - self.ending_stats[ending][pos_d]
-					if np.random.hypergeometric(ngood, nbad, 1) == 1:
-						self.positives += 1
-					else:
-						self.negatives += 1
-				except KeyError:
-					continue
 
-		return f'Accuracy of the model is: {round((self.positives/(self.positives + self.negatives) * 100), 2)} %'
+
+
+
+
